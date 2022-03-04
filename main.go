0b00c21deca2084aed744fcd231733b4c70339a9
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-03-04 15:35:49 +0700

    case,condition

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/xvbnm48/fundamental_golang/calculation"
 )
 
@@ -19,4 +20,34 @@ func main() {
 	var nama string = "sakura miyawaki"
 	fmt.Println(nama)
 
+	// age := 50
+	// if age > 10 {
+	// 	fmt.Println("age is greater than 10")
+	// } else {
+	// 	fmt.Println("age is less than 10")
+	// }
+
+	score := 80
+	var grade string
+	if score <= 50 {
+		grade = "E"
+	} else if score >= 60 {
+		grade = "A"
+	} else {
+		grade = "C"
+	}
+
+	fmt.Println(grade)
+
+	nilai := 3
+	switch nilai {
+	case 1:
+		fmt.Println("nilai 1")
+	case 2:
+		fmt.Println("nilai 2")
+	case 3:
+		fmt.Println("nilai 3")
+	default:
+		fmt.Println("nilai tidak diketahui")
+	}
 }
